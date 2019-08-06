f8423042213f7b91682ab28bad86ac2cbca81037
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2019-08-06 02:58:57 +0200

    Make it to pwa

---
+++ config/manifest.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = function(/* environment, appConfig */) {
+  // See https://zonkyio.github.io/ember-web-app for a list of
+  // supported properties
+
+  return {
+    name: "DoTheMathRight",
+    short_name: "DoTheMathRight",
+    description: "",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#fff",
+    theme_color: "#fff",
+    icons: [
+      {
+        src: "https://i.imgur.com/wFEVgLe.png",
+        sizes: "540x540",
+        type: "image/png"
+      }
+    ],
+    ms: {
+      tileColor: '#fff'
+    }
+  };
+}
